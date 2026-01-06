0e877f1b7389f275e4b61c96338a284bcff3cde8
astone <astone@B108-TS>
2026-01-06 12:21:06 +0000

    Add project files.

---
+++ OOPCharacter12B26/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OOPCharacter12B26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Character player1 = new Character(100);
+            player1.changeHealth(-50);
+
+        }
+    }
+    class Character
+    {
+        private int health;  // private can't access outside of the class by the object directly
+
+        public Character(int startHealth) // Instantiation - To be explained later (Called with new)
+        {
+            health = startHealth;
+        }
+        public void changeHealth(int change)// public allows to be called by the object created
+        {
+            health += change;
+            if(health > 100)
+            {
+                health = 100;
+            }
+        }
+    }
+
+}
